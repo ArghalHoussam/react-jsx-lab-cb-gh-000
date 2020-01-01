8832722e234fe22f41c55021ccd0010f3dd6eaf7
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-01 19:24:26 +0000

    Automatically backed up by Learn

---
+++ src/components/Webpage.js
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react';
+// import FillerText from './FillerText';
+
+export class Webpage extends Component {
+  render() {
+    return (
+      <h1>{ "The world's coolest webpage" }</h1>
+      <FillerText />
+      <FillerText />
+    )
+  }
+}
