6e1b28e733bfc018b1703bf5b68639f64b1806b4
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-01 19:26:16 +0000

    Automatically backed up by Learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FillerText from './FillerText';
 
-export class Webpage extends Component {
+export class Webpage extends React.Component {
   render() {
     return (
       <h1>{ "The world's coolest webpage" }</h1>
